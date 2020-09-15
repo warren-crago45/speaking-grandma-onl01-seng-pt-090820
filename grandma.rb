b51d1346e5a76474f4ffee576e15cc2bcc31ef46
Warren Crago <warren.crago45@gmail.com>
2020-09-15 19:38:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,16 +1,5 @@
 # Write a speak_to_grandma method.
-requires 'pry'
-def speak_to_grandma
-  greeting = "Hi Nana, how are you?"
-  if greeting != "Hi Grandma, it's nice to see you again!".upcase
-    return "HUH?! SPEAK UP SONNY!"
-    binding.pry
-  elsif greeting == "Hi Grandma, it's nice to see you again!".upcase
-    return "NO, NO SINCE 1938!"
-  elsif greeting == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN"
-  end
-end
+
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
